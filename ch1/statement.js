26c4e3bc0ed3dc636a13e499a255ecb942858869
roy-jung <power4ce@gmail.com>
2021-07-13 19:05:10 +0900

    ch1 complete

--- ch1/statement.js
+++ ch1/statement.js
@@ -1,41 +1,41 @@
-const statement = (invoice, plays) => {
-  let totalAmount = 0
-  let volumeCredits = 0
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`
-  const format = new Intl.NumberFormat('en-US', {
+import createStatementData from './createStatementData.js'
+
+const usd = aNumber =>
+  new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
-  }).format
+  }).format(aNumber / 100)
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
-    let thisAmount = 0
-
-    switch (play.type) {
-      case 'tragedy': {
-        thisAmount = 40000
-        if (perf.audience > 30) thisAmount += 1000 * (perf.audience - 30)
-        break
-      }
-      case 'comedy': {
-        thisAmount = 30000
-        if (perf.audience > 20) thisAmount += 10000 + 500 * (perf.audience - 20)
-        thisAmount += 300 * perf.audience
-        break
-      }
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`)
-    }
-
-    volumeCredits += Math.max(perf.audience - 30, 0)
-    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
-    totalAmount += thisAmount
+const renderPlainText = data => {
+  let result = `청구 내역 (고객명: ${data.customer})\n`
+  for (let perf of data.performances) {
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`
   }
-  result += `총액: ${format(totalAmount / 100)}\n`
-  result += `적립 포인트: ${volumeCredits}점\n`
+  result += `총액: ${usd(data.totalAmount)}\n`
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`
   return result
 }
 
-export default statement
+const renderHtml = data => {
+  let result = `
+<h1>청구 내역 (고객명: ${data.customer})</h1>
+<table>
+  <tr><th>연극</th><th>좌석수</th><th>금액</th></tr>`
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td><td>${usd(perf.amount)}</td></tr>`
+  }
+  result += `</table>
+<p>총액: <em>${usd(data.totalAmount)}</em></p>
+<p>적립 포인트: <em>${data.totalVolumeCredits}점</em></p>
+`
+  return result
+}
+
+export const statement = (invoice, plays) => {
+  return renderPlainText(createStatementData(invoice, plays))
+}
+
+export const htmlStatement = (invoice, plays) => {
+  return renderHtml(createStatementData(invoice, plays))
+}
