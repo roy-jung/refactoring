466609ea7a11613f2d4a2f3e59f779828227d481
ByeongSooLee <djaxornwkd12@gmail.com>
2023-08-02 22:58:05 +0900

    refactor: [ch7] 레코드 캡슐화하기 01

--- ch7/01-1.js
+++ ch7/01-1.js
@@ -1,7 +1,31 @@
-let result = ''
-const organization = { name: '애크미 구스베리', country: 'GB' }
+class Organization {
+  #name;
+  #country;
+  constructor(data) {
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(aString) {
+    this.#name = aString;
+  }
+  get country() {
+    return this.#country;
+  }
+  set country(aCountryCode) {
+    this.#country = aCountryCode;
+  }
+}
 
-organization.name = '애그머니 블루베리'
-result += `<h1>${organization.name}</h1>`
+let result = '';
+const organization = new Organization({
+  name: '애크미 구스베리',
+  country: 'GB',
+});
 
-console.log(result)
+const getOrganization = () => organization;
+
+getOrganization().name = '애그머니 블루베리';
+result += `<h1>${getOrganization().name}</h1>`;
