51d6ed4de945de9f4eb10b6642e9baa737c27e69
ByeongSooLee <djaxornwkd12@gmail.com>
2023-08-02 23:00:02 +0900

    refactor: [ch7] 임시변수를 질의함수로 바꾸기

--- ch7/04.js
+++ ch7/04.js
@@ -1,16 +1,23 @@
 class Order {
-  _quantity
-  _item
+  _quantity;
+  _item;
   constructor(quantity, item) {
-    this._quantity = quantity
-    this._item = item
+    this._quantity = quantity;
+    this._item = item;
   }
   get price() {
-    let basePrice = this._quantity * this._item.price
-    let discountFactor = 0.98
-    if (basePrice > 1000) discountFactor -= 0.03
-    return basePrice * discountFactor
+    return this.basePrice * this.discountFactor;
+  }
+
+  get discountFactor() {
+    let discountFactor = 0.98;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return discountFactor;
+  }
+
+  get basePrice() {
+    return this._quantity * this._item.price;
   }
 }
-const order = new Order(10, { price: 1000 })
-console.log(order.price)
+const order = new Order(10, { price: 1000 });
+console.log(order.price);
