9c0a1882afb412f5176d63f69dbb9927162d6bf5
ByeongSooLee <djaxornwkd12@gmail.com>
2023-08-02 23:02:03 +0900

    refactor: [ch7] 중개자 제거하기

--- ch7/08.js
+++ ch7/08.js
@@ -1,27 +1,28 @@
 class Person {
-  _name
-  _department
+  _name;
+  _department;
   constructor(name, department) {
-    this._name = name
-    this._department = department
+    this._name = name;
+    this._department = department;
   }
   get name() {
-    return this._name
+    return this._name;
   }
-  get manager() {
-    return this._department.manager
+  get department() {
+    return this._department;
   }
 }
 
 class Department {
-  _manager
+  _manager;
   constructor(manager) {
-    this._manager = manager
+    this._manager = manager;
   }
+
   get manager() {
-    return this._manager
+    return this._manager;
   }
 }
 
-const jn = new Person('재남', new Department('로이'))
-console.log(jn.manager)
+const jn = new Person('재남', new Department('로이'));
+console.log(jn.department.manager);
