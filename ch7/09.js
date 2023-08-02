829c6b1185ee366b5d41b796a66ef69103b3f0bc
ByeongSooLee <djaxornwkd12@gmail.com>
2023-08-02 23:02:21 +0900

    refactor: [ch7] 알고리즘 교체하기

--- ch7/09.js
+++ ch7/09.js
@@ -1,9 +1,7 @@
-const findPerson = people => {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') return 'Don'
-    if (people[i] === 'John') return 'John'
-    if (people[i] === 'Kent') return 'Kent'
-  }
-  return ''
-}
-console.log(findPerson(['Roy', 'Jay', 'Don', 'Kay', 'John', 'Peter', 'Kent', 'Clark']))
+const findPerson = (people) => {
+  const candidates = ['Don', 'John', 'Kent'];
+  return people.find((p) => candidates.includes(p)) || '';
+};
+console.log(
+  findPerson(['Roy', 'Jay', 'Don', 'Kay', 'John', 'Peter', 'Kent', 'Clark'])
+);
