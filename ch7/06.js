aff361785ff35bc8cb504445fb7e6b84b75b46e1
ByeongSooLee <djaxornwkd12@gmail.com>
2023-08-02 23:01:16 +0900

    refactor: [ch7] 클래스 인라인하기

--- ch7/06.js
+++ ch7/06.js
@@ -1,44 +1,28 @@
-class TrackingInformation {
-  _shippingCompany
-  _trackingNumber
+class Shipment {
+  _shippingCompany;
+  _trackingNumber;
   get shippingCompany() {
-    return this._shippingCompany
+    return this._shippingCompany;
   }
   set shippingCompany(arg) {
-    this._shippingCompany = arg
+    this._shippingCompany = arg;
   }
   get trackingNumber() {
-    return this._trackingNumber
+    return this._trackingNumber;
   }
   set trackingNumber(arg) {
-    this._trackingNumber = arg
-  }
-  get display() {
-    return `${this.shippingCompany}: ${this.trackingNumber}`
-  }
-}
-
-class Shipment {
-  _trackingInformation
-  constructor() {
-    this._trackingInformation = new TrackingInformation()
+    this._trackingNumber = arg;
   }
   get trackingInfo() {
-    return this._trackingInformation.display
-  }
-  get trackingInformation() {
-    return this._trackingInformation
-  }
-  set trackingInformation(aTrackingInformation) {
-    this._trackingInformation = aTrackingInformation
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
 }
 
 const client1 = () => {
-  const aShipment = new Shipment()
-  const vendor = { name: 'A-SHIP', number: '010-1234-5678' }
-  aShipment.trackingInformation.shippingCompany = vendor.name
-  aShipment.trackingInformation.trackingNumber = vendor.number
-  return aShipment.trackingInfo
-}
-console.log(client1())
+  const aShipment = new Shipment();
+  const vendor = { name: 'A-SHIP', number: '010-1234-5678' };
+  aShipment._shippingCompany = vendor.name;
+  aShipment._trackingNumber = vendor.number;
+  return aShipment.trackingInfo;
+};
+console.log(client1());
