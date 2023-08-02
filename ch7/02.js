0f5a18c0eb0db83d323a09476083c084c6cc2b3d
ByeongSooLee <djaxornwkd12@gmail.com>
2023-08-02 22:59:08 +0900

    refactor: [ch7] 컬렉션 캡슐화 하기

--- ch7/02.js
+++ ch7/02.js
@@ -2,55 +2,67 @@ const COURSES = {
   korean: { basic: 'korean', advanced: 'korean advanced' },
   english: { basic: 'english', advanced: 'english advanced' },
   mathematics: { basic: 'mathematics', advanced: 'mathematics advanced' },
-}
+};
 
 class Person {
-  _name = ''
-  _courses = []
+  _name = '';
+  _courses = [];
   constructor(name) {
-    this._name = name
+    this._name = name;
   }
   get name() {
-    return this._name
+    return this._name;
   }
   get courses() {
-    return this._courses
+    return [...this._courses];
   }
   set courses(aList) {
-    this._courses = aList
+    this._courses = [...aList];
+  }
+  addCourses(aCourse) {
+    this._courses.push(aCourse);
+  }
+  removeCourses(
+    aCourse,
+    fnIfAbsent = () => {
+      throw new RangeError();
+    }
+  ) {
+    const index = this._courses.indexOf(aCourse);
+    if (index === -1) fnIfAbsent();
+    else this._courses.splice(index, 1);
   }
 }
 
 class Course {
-  _name = ''
-  _isAdvanced = false
+  _name = '';
+  _isAdvanced = false;
   constructor(name, isAdvanced) {
-    this._name = name
-    this._isAdvanced = isAdvanced
+    this._name = name;
+    this._isAdvanced = isAdvanced;
   }
   get name() {
-    return this._name
+    return this._name;
   }
   get isAdvanced() {
-    return this._isAdvanced
+    return this._isAdvanced;
   }
 }
 
-const readBasicCourseNames = filename => Object.values(filename).map(c => c.basic)
+const readBasicCourseNames = (filename) =>
+  Object.values(filename).map((c) => c.basic);
 
 const client1 = () => {
-  const aPerson = new Person('파울러')
+  const aPerson = new Person('파울러');
 
-  const numAdvancedCourses = aPerson.courses.filter(c => c.isAdvanced).length
+  const numAdvancedCourses = aPerson.courses.filter((c) => c.isAdvanced).length;
 
-  const basicCourseNames = readBasicCourseNames(COURSES)
-  aPerson.courses = basicCourseNames.map(name => new Course(name, false))
+  const basicCourseNames = readBasicCourseNames(COURSES);
+  aPerson.courses = basicCourseNames.map((name) => new Course(name, false));
 
-  return aPerson
-
-  //
-  // for(const name of readBasicCourseNames(COURSES)) {
-  //   aPerson.courses.push(new Course(name, false))
-  // }
-}
-console.log(client1())
+  for (const name of readBasicCourseNames(COURSES)) {
+    aPerson.addCourses(new Course(name, false));
+  }
+  return aPerson;
+};
+console.log(client1());
