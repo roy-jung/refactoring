ffbf6da63d513f65ba12dd0e8722f11e8c7e173b
ByeongSooLee <djaxornwkd12@gmail.com>
2023-08-02 22:58:41 +0900

    refactor: [ch7] 레코드 캡슐화하기 02

--- ch7/01-2.js
+++ ch7/01-2.js
@@ -1,14 +1,22 @@
-import { readJSON } from '../fileController.js'
+import { readJSON } from '../fileController.js';
 
-const customerData = readJSON('ch7/01-2.json')
+const customerData = readJSON('ch7/01-2.json');
+
+const getRawDataOfCustomers = () => {
+  return customerData;
+};
+const setRawDataOfCustomers = (arg) => {
+  customerData = arg;
+};
 
 export const writeData = (customerId, year, month, amount) => {
-  customerData[customerId].usages[year][month] = amount
-}
+  getRawDataOfCustomers()[customerId].usages[year][month] = amount;
+};
 
 export const compareUsage = (customerId, laterYear, month) => {
-  const later = customerData[customerId].usages[laterYear][month]
-  const earlier = customerData[customerId].usages[laterYear - 1][month]
-  return { laterAmount: later, change: later - earlier }
-}
-export const getCustomer = () => customerData
+  const later = getRawDataOfCustomers()[customerId].usages[laterYear][month];
+  const earlier =
+    getRawDataOfCustomers()[customerId].usages[laterYear - 1][month];
+  return { laterAmount: later, change: later - earlier };
+};
+export const getCustomer = () => customerData;
